3d6e09eb00b191c2f44bef254b4b8f38498a5ad6
Yegor Shytikov <egorshitiov@gmail.com>
2021-05-24 21:28:43 -0700

    initial

---
+++ Genaker/FixIndexer/Plugin/Magento/Catalog/Model/Indexer/Product/Price.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Genaker\FixIndexer\Plugin\Magento\Catalog\Model\Indexer\Product;
+
+class Price
+{
+
+    public function aroundExecute(
+        \Magento\Catalog\Model\Indexer\Product\Price $subject,
+        \Closure $proceed,
+        $ids
+    ) {
+    
+        $hour = date('G'); // 0 .. 23
+	// Show run between 9am and 5pm
+	if ($hour <= 4) {
+		$result = $proceed($ids);
+        	return $result;
+        }
+    }
+}
+
